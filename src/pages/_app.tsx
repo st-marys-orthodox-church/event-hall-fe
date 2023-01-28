ce6a003a9a629c85a5c7c7374766ff42f6078706
Angello Lazar <angellolazar@Angellos-Air.attlocal.net>
2023-01-28 11:41:05 -0500

    fixed build issue

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,9 @@ import { AppProps } from 'next/app';
 
 import '../styles/global.css';
 
-const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  const AnyComponent = Component as any;
+  return (<AnyComponent {...pageProps} />)
+};
 
 export default MyApp;
