e3e9c6aec9fc00ecf53a0b0396a5f771f4f7ae81
Adrian Albescu <0xyoyobojo@gmail.com>
2023-03-12 22:00:48 -0400

    changed email and email dns

--- src/pages/api/sendgrid.ts
+++ src/pages/api/sendgrid.ts
@@ -5,31 +5,30 @@ sendgrid.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY as string);
 
 async function sendEmail(req: NextApiRequest, res: NextApiResponse) {
   try {
-    if (process.env.NEXT_PUBLIC_DEV) {
+    // if (process.env.NEXT_PUBLIC_DEV) {
       console.log('REQ.BODY', req.body);
-    } else {
+    // } else {
       await sendgrid.send({
-        to: 'saintmaryro.org@gmail.com', // Your email where you'll receive emails
-        from: 'saintmaryro.org@gmail.com', // your website email address here
+        to: 'eventhall@saintmaryro.org', // Your email where you'll receive emails
+        from: 'eventhall@saintmaryro.org', // your website email address here
         subject: `Inquiry for Fellowship Event Hall`,
         html: `
           <div>
             <h1>New Inquiry from ${req.body.name}</h1>
             <div style="color: #7f7f7f; font-size: 12px; display: flex; flex-direction: column; gap: 6px;">
-              <span>Email Address: ${req.body.email}</span>
+              <span>Email Address: ${req.body.email}</span><br />
               <span>Date Sent: ${new Date(
                 req.body.date
               ).toLocaleString()}</span>
             </div>
             <div style="display: flex; flex-direction: column; margin-top: 32px; gap: 6px;">
-              <span>Package: ${req.body.package}</span>
-              <span>Capacity: ${req.body.cap}</span>
+              <span>Capacity: ${req.body.cap}</span><br />
               <span>Message: ${req.body.message}</span>
             </div>
           </div>
         `,
       });
-    }
+    // }
   } catch (error: any) {
     console.log(error);
     return res.status(error.statusCode || 500).json({ error: error.message });
