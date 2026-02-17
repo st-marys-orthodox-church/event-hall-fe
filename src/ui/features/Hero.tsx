509fba6361d03b8cc37e45693808d418e13d36e4
Slimshady <slimshady@Slimshadys-Mac-mini.local>
2026-02-16 23:35:28 -0500

    fix: move scroll indicator to bottom of hero to avoid button overlap

--- src/ui/features/Hero.tsx
+++ src/ui/features/Hero.tsx
@@ -66,16 +66,16 @@ const Hero = () => {
                 </ModernButton>
               </Link>
             </div>
-
-            {/* Scroll Indicator */}
-            <div className="absolute bottom-8 left-1/2 -translate-x-1/2 animate-bounce">
-              <div className="w-6 h-10 border-2 border-white/50 rounded-full flex justify-center pt-2">
-                <div className="w-1.5 h-3 bg-white/70 rounded-full" />
-              </div>
-            </div>
           </div>
         </FadeIn>
       </Section>
+
+      {/* Scroll Indicator - positioned at bottom of hero */}
+      <div className="absolute bottom-4 left-1/2 -translate-x-1/2 animate-bounce z-[2]">
+        <div className="w-6 h-10 border-2 border-white/50 rounded-full flex justify-center pt-2">
+          <div className="w-1.5 h-3 bg-white/70 rounded-full" />
+        </div>
+      </div>
     </div>
   );
 };
